6322dc1a1d3f7e12b6344c711ceeca3acc83a51c
kmaitreys <km.maitrey@gmail.com>
2025-07-01 21:51:20 +0530

    [impl] remove orphan module

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
+//! The Planetary Love Numbers Calculator
+//!
+//!
+
 pub mod config;
 pub mod constants;
 pub mod core;
 pub mod io;
 pub mod matrix;
 pub mod messages;
-pub mod model;
 pub mod numeric;
 pub mod utils;
